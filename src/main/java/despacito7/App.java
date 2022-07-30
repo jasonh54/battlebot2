decd44fbb5330116728b452c83ce51c002e4c404
Steve Scruff <mutantpiggiegolems@gmail.com>
2022-07-30 11:50:14 -0700

    rendering online

--- src/main/java/despacito7/App.java
+++ src/main/java/despacito7/App.java
@@ -1,7 +1,35 @@
 package despacito7;
 
+import java.awt.Graphics2D;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import javax.swing.JFrame;
+
 public class App {
+    public static final JFrame f = new JFrame("Battlebot");
+    public static final DrawingCanvas dc = new DrawingCanvas(f);
+
+    private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        f.setVisible(true);
+        f.add(dc);
+        f.addKeyListener(dc);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        f.setFocusable(true);
+        // f.setIconImage(Utils.ICONIMG);
+        f.setResizable(false);
+
+        executor.scheduleAtFixedRate(App::tick, 0, (long) (1000 / Constants.TPS), java.util.concurrent.TimeUnit.MILLISECONDS);
+    }
+
+    public static void render(Graphics2D g2) {
+        g2.drawString("Hello World", 100, 100);
+    }
+
+    public static void tick() {
+
     }
 }
---
+++ src/main/java/despacito7/DrawingCanvas.java
@@ -0,0 +1,59 @@
+package despacito7;
+
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class DrawingCanvas extends javax.swing.JComponent implements KeyListener {
+    public JFrame parent;
+    public long prevtime = 0;
+    private boolean draw = true;
+
+    public DrawingCanvas(JFrame f) {
+        this.parent = f;
+        this.setFocusable(true);
+        this.requestFocus();
+        this.setBackground(java.awt.Color.GRAY);
+    }
+
+    public void startDraw() { // begin the draw loop!
+        while (this.draw) {
+            if (!this.parent.isShowing())
+                continue; // dont render in background
+            if (System.currentTimeMillis() - this.prevtime >= 1000.0 / Constants.FPS) {
+                this.repaint();
+                this.prevtime = System.currentTimeMillis();
+            }
+        }
+    }
+
+    public void stopDraw() {
+        this.draw = false;
+    }
+
+    @Override
+    public void paintComponent(java.awt.Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        App.render(g2);
+
+        g.dispose();
+        g2.dispose();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+}
