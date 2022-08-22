fc400b760dae65911f73cf3f16e64e018396f68d
Jason Huang <jahuang13@gmail.com>
2022-08-21 19:07:48 -0700

    bugged image loading

--- src/main/java/despacito7/App.java
+++ src/main/java/despacito7/App.java
@@ -1,10 +1,15 @@
 package despacito7;
 
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.File;
+// import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
@@ -25,7 +30,25 @@ public class App {
     public static final Gson gson = new Gson();
     private static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
+    public static HashMap<String,Image> itemImages = new HashMap<String,Image>();
+    public static HashMap<String,Image> monsterImages = new HashMap<String,Image>();
+    public static Image[] tileImages;
+
     public static void main(String[] args) {
+        String resourcesDirectoryPath = Paths.get("").toAbsolutePath().toString() + "/src/main/resources/images";
+        System.out.println(resourcesDirectoryPath);
+        File imageDirectory = new File(resourcesDirectoryPath+"/items");
+        File[] imageDirectories = imageDirectory.listFiles();
+        for(File object: imageDirectories){
+            if(object.isFile()){
+                System.out.println(object.getName());
+                String filename = object.getName();
+                itemImages.put(filename.substring(0,filename.length()-4), instance.loadImage(filename));
+            }
+        }
+
+
+
         instance.init();
 
         f.setVisible(true);
