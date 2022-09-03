edcdbf7a700635ae13a6aeb0fce089e2bb07dfee
Steve Scruff <mutantpiggiegolems@gmail.com>
2022-09-03 11:34:12 -0700

    fix my other commit

---
+++ src/main/java/despacito7/detail/Item.java
@@ -0,0 +1,10 @@
+package despacito7.detail;
+
+import despacito7.util.Coord;
+// import despacito7.util.GameObject;
+
+public class Item { //extends GameObject
+    public Item(String id, Coord coord) {
+        
+    }
+}
---
+++ src/main/java/despacito7/detail/NPC.java
@@ -0,0 +1,9 @@
+package despacito7.detail;
+
+// import despacito7.util.AnimatingObject;
+
+public class NPC { // extends AnimatingObject 
+    public NPC(String id) {
+        
+    }
+}
