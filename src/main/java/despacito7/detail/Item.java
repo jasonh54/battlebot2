5ab0bf02dc29986682be24958a9a787c04bdf491
cyndaquil8 <ivy@mahncke.org>
2022-10-22 11:09:21 -0700

    baseline NPC functions

--- src/main/java/despacito7/detail/Item.java
+++ src/main/java/despacito7/detail/Item.java
@@ -4,7 +4,7 @@ import despacito7.util.Coord;
 // import despacito7.util.GameObject;
 
 public class Item { //extends GameObject
-    public Item(String id, Coord coord) {
+    public Item(String id) {
         
     }
 }
